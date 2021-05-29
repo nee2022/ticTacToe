4dac40ac543826f9c4e245ea0d3610ed4e8d78f9
zhangyihua <642850742@qq.com>
2021-05-29 10:23:32 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './Game.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Game></Game>
+    </div>
+  );
+}
+
+export default App;
